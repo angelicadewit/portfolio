5d64960207093ecc1024832049e18541910fb7a0
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-03-14 18:49:43 -0400

    modifed more work

--- dist/js/workhover.js
+++ dist/js/workhover.js
@@ -3,9 +3,23 @@
 var hover = document.querySelector(".border");
 
 hover.addEventListener("mouseover", function (e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
+    var offsetX = e.offsetX;
+    var offsetY = e.offsetY;
 
-    console.log(x, y);
+    TweenMax.to(hover, 2, {
+        x: offsetX + 5,
+        y: offsetY + 5
+    });
+});
+
+hover.addEventListener("mouseout", function (e) {
+    console.log("mouseout");
+    var offsetX = e.offsetX;
+    var offsetY = e.offsetY;
+
+    TweenMax.to(hover, 2, {
+        x: 0,
+        y: 0
+    });
 });
 //# sourceMappingURL=workhover.js.map
--- src/js/workhover.js
+++ src/js/workhover.js
@@ -1,9 +1,23 @@
 let hover = document.querySelector(`.border`)
 
 hover.addEventListener(`mouseover`, function(e){
-    let x =  e.offsetX
-    let y = e.offsetY
+    let offsetX =  e.offsetX
+    let offsetY = e.offsetY
     
-    console.log(x,y)
+    TweenMax.to(hover, 2, {
+        x: offsetX + 5,
+        y: offsetY + 5,
+    });
+});
+
+hover.addEventListener(`mouseout`, function(e){
+    console.log(`mouseout`)
+    let offsetX =  e.offsetX
+    let offsetY = e.offsetY
+    
+    TweenMax.to(hover, 2, {
+        x: 0,
+        y: 0
+    });
 });
 

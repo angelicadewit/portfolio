5c0c05d2385b5efb3a52fbbcd74de65899053f41
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-03-16 00:06:38 -0400

    added more css and js to work

--- dist/js/workhover.js
+++ dist/js/workhover.js
@@ -1,29 +1,57 @@
 "use strict";
 
-var hover = document.querySelector(".border");
-var buttonGroup = document.querySelector(".buttons");
+var borders = document.querySelectorAll(".border");
 
-hover.addEventListener("mouseover", function (e) {
-    var offsetX = e.offsetX;
-    var offsetY = e.offsetY;
+borders.forEach(function (border) {
+    border.addEventListener("mousemove", function (e) {
+        var offsetX = e.offsetX;
+        var offsetY = e.offsetY;
+        var height = e.target.height / 2;
+        var width = e.target.width / 2;
 
-    var buttonOffsetX = buttonGroup.offsetX;
-    var buttonOffsetY = buttonGroup.offsetY;
+        var radians = Math.atan2(offsetX, offsetY);
+        var degree = radians * (180 / Math.PI) * -1 + 90;
 
-    TweenMax.to(hover, 2, {
-        x: offsetX + 5,
-        y: offsetY + 5
+        // var dx = offsetX - origin.x;
+        // var dy = offsetY - origin.y;
+
+
+        var fractionX = offsetX / width - 0.5;
+        var fractionY = offsetY / height - 0.5;
+
+        TweenMax.to(this.children[0], 0.5, {
+            x: (width - offsetX) / 2,
+            y: (height - offsetY) / 2,
+            // rotationZ:  degree,
+            // transformOrigin: "5% 75%",
+            ease: Linear.easeNone
+        });
+
+        TweenMax.to(this.children[1], 2, {
+            x: (width - offsetX) * -1,
+            y: (height - offsetY) * -1,
+            ease: Linear.easeNone
+        });
     });
 });
 
-hover.addEventListener("mouseout", function (e) {
-    console.log("mouseout");
-    var offsetX = e.offsetX;
-    var offsetY = e.offsetY;
+borders.forEach(function (border) {
+    border.addEventListener("mouseout", function (e) {
+        console.log("mouseout");
 
-    TweenMax.to(hover, 2, {
-        x: 0,
-        y: 0
+        TweenMax.to(this.children[0], 0.5, {
+            x: 0,
+            y: 0,
+            rotation: 0,
+            transformOrigin: 0,
+            ease: Linear.easeNone
+        });
+
+        TweenMax.to(this.children[1], 0.5, {
+            x: 0,
+            y: 0,
+            ease: Linear.easeNone
+        });
     });
 });
 //# sourceMappingURL=workhover.js.map
--- src/js/workhover.js
+++ src/js/workhover.js
@@ -1,28 +1,58 @@
-let hover = document.querySelector(`.border`)
-let buttonGroup = document.querySelector(`.buttons`)
+let borders = document.querySelectorAll(`.border`)
 
-hover.addEventListener(`mouseover`, function(e){
-    let offsetX =  e.offsetX
-    let offsetY = e.offsetY
-
-    let buttonOffsetX = buttonGroup.offsetX
-    let buttonOffsetY = buttonGroup.offsetY
-
+borders.forEach(function(border){
+    border.addEventListener(`mousemove`, function(e){
+        let offsetX =  e.offsetX
+        let offsetY = e.offsetY
+        let height = e.target.height / 2
+        let width = e.target.width / 2
     
-    TweenMax.to(hover, 2, {
-        x: offsetX + 5,
-        y: offsetY + 5,
-    });
-});
-
-hover.addEventListener(`mouseout`, function(e){
-    console.log(`mouseout`)
-    let offsetX =  e.offsetX
-    let offsetY = e.offsetY
     
-    TweenMax.to(hover, 2, {
-        x: 0,
-        y: 0
+        let radians = Math.atan2(offsetX, offsetY);
+        let degree = (radians * (180 / Math.PI) * -1) + 90; 
+    
+        // var dx = offsetX - origin.x;
+        // var dy = offsetY - origin.y;
+    
+    
+        let fractionX = (offsetX / width) - 0.5
+        let fractionY = (offsetY / height) - 0.5
+        
+        TweenMax.to(this.children[0], 0.5, {
+            x: (width - offsetX) / 2,
+            y: (height - offsetY) / 2,
+            // rotationZ:  degree,
+            // transformOrigin: "5% 75%",
+            ease: Linear.easeNone,
+        });
+    
+        TweenMax.to(this.children[1], 2, {
+            x: (width - offsetX) * -1,
+            y: (height - offsetY) * -1,
+            ease: Linear.easeNone,
+        });
     });
-});
+})
+
+borders.forEach(function(border){
+    border.addEventListener(`mouseout`, function(e){
+        console.log(`mouseout`)
+    
+        TweenMax.to(this.children[0], 0.5, {
+            x: 0,
+            y: 0,
+            rotation: 0,
+            transformOrigin: 0,
+            ease: Linear.easeNone,
+        });
+    
+        TweenMax.to(this.children[1], 0.5, {
+            x: 0,
+            y: 0,
+            ease: Linear.easeNone,
+        });
+    });
+})
+
+
 
